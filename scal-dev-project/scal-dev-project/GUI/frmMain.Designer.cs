301398c0da62191c1e9308dfa24212e2f41fef7f
Julio Eduardo Canizalez Salinas <juliocanizalez@outlook.com>
2020-05-28 00:34:15 -0600

    update

--- scal-dev-project/scal-dev-project/GUI/frmMain.Designer.cs
+++ scal-dev-project/scal-dev-project/GUI/frmMain.Designer.cs
@@ -29,43 +29,55 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-            this.btnDescriptiva = new System.Windows.Forms.Button();
-            this.topPanel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
             this.parentPanel = new System.Windows.Forms.Panel();
             this.topPanel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnMinimize = new System.Windows.Forms.Button();
+            this.topPanel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panelSubDescriptiva = new System.Windows.Forms.Panel();
-            this.topPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
+            this.btnDescriptiva = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.panel4.SuspendLayout();
+            this.topPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelSubDescriptiva.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnDescriptiva
+            // panel4
             // 
-            this.btnDescriptiva.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
-            this.btnDescriptiva.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnDescriptiva.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
-            this.btnDescriptiva.FlatAppearance.BorderSize = 0;
-            this.btnDescriptiva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDescriptiva.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDescriptiva.ForeColor = System.Drawing.Color.White;
-            this.btnDescriptiva.Location = new System.Drawing.Point(0, 0);
-            this.btnDescriptiva.Name = "btnDescriptiva";
-            this.btnDescriptiva.Size = new System.Drawing.Size(248, 40);
-            this.btnDescriptiva.TabIndex = 0;
-            this.btnDescriptiva.Text = "Analizar una variable";
-            this.btnDescriptiva.UseVisualStyleBackColor = false;
-            this.btnDescriptiva.Click += new System.EventHandler(this.btnDescriptiva_Click);
+            this.panel4.Controls.Add(this.parentPanel);
+            this.panel4.Controls.Add(this.topPanel2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(248, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(760, 561);
+            this.panel4.TabIndex = 15;
+            // 
+            // parentPanel
+            // 
+            this.parentPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.parentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.parentPanel.Location = new System.Drawing.Point(0, 31);
+            this.parentPanel.Name = "parentPanel";
+            this.parentPanel.Size = new System.Drawing.Size(760, 530);
+            this.parentPanel.TabIndex = 1;
+            // 
+            // topPanel2
+            // 
+            this.topPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.topPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel2.Location = new System.Drawing.Point(0, 0);
+            this.topPanel2.Name = "topPanel2";
+            this.topPanel2.Size = new System.Drawing.Size(760, 31);
+            this.topPanel2.TabIndex = 0;
+            this.topPanel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel2_MouseDown);
             // 
             // topPanel1
             // 
@@ -87,20 +99,33 @@
             this.panel2.Size = new System.Drawing.Size(602, 30);
             this.panel2.TabIndex = 15;
             // 
-            // panel1
+            // btnClose
             // 
-            this.panel1.AutoScroll = true;
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
-            this.panel1.Controls.Add(this.panelSubDescriptiva);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.topPanel1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.ImeMode = System.Windows.Forms.ImeMode.On;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(248, 600);
-            this.panel1.TabIndex = 14;
+            this.btnClose.BackgroundImage = global::scal_dev_project.Properties.Resources.icons8_macos_close_30;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(8, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackgroundImage = global::scal_dev_project.Properties.Resources.icons8_macos_minimize_30_2_;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Location = new System.Drawing.Point(31, 6);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(20, 20);
+            this.btnMinimize.TabIndex = 16;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
             // 
             // panel3
             // 
@@ -112,34 +137,15 @@
             this.panel3.Size = new System.Drawing.Size(248, 257);
             this.panel3.TabIndex = 14;
             // 
-            // panel4
+            // pictureBox1
             // 
-            this.panel4.Controls.Add(this.parentPanel);
-            this.panel4.Controls.Add(this.topPanel2);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(248, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(760, 600);
-            this.panel4.TabIndex = 15;
-            // 
-            // parentPanel
-            // 
-            this.parentPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
-            this.parentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.parentPanel.Location = new System.Drawing.Point(0, 31);
-            this.parentPanel.Name = "parentPanel";
-            this.parentPanel.Size = new System.Drawing.Size(760, 569);
-            this.parentPanel.TabIndex = 1;
-            // 
-            // topPanel2
-            // 
-            this.topPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
-            this.topPanel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.topPanel2.Location = new System.Drawing.Point(0, 0);
-            this.topPanel2.Name = "topPanel2";
-            this.topPanel2.Size = new System.Drawing.Size(760, 31);
-            this.topPanel2.TabIndex = 0;
-            this.topPanel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel2_MouseDown);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -160,44 +166,6 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(40, 38);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(164, 164);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // btnMinimize
-            // 
-            this.btnMinimize.BackgroundImage = global::scal_dev_project.Properties.Resources.icons8_macos_minimize_30_2_;
-            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.Location = new System.Drawing.Point(31, 6);
-            this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(20, 20);
-            this.btnMinimize.TabIndex = 16;
-            this.btnMinimize.UseVisualStyleBackColor = true;
-            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackgroundImage = global::scal_dev_project.Properties.Resources.icons8_macos_close_30;
-            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnClose.FlatAppearance.BorderSize = 0;
-            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.Location = new System.Drawing.Point(8, 6);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(20, 20);
-            this.btnClose.TabIndex = 15;
-            this.btnClose.UseVisualStyleBackColor = true;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
             // panelSubDescriptiva
             // 
             this.panelSubDescriptiva.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
@@ -205,8 +173,40 @@
             this.panelSubDescriptiva.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSubDescriptiva.Location = new System.Drawing.Point(0, 328);
             this.panelSubDescriptiva.Name = "panelSubDescriptiva";
-            this.panelSubDescriptiva.Size = new System.Drawing.Size(248, 100);
+            this.panelSubDescriptiva.Size = new System.Drawing.Size(248, 48);
             this.panelSubDescriptiva.TabIndex = 16;
+            // 
+            // btnDescriptiva
+            // 
+            this.btnDescriptiva.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.btnDescriptiva.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDescriptiva.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnDescriptiva.FlatAppearance.BorderSize = 0;
+            this.btnDescriptiva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDescriptiva.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescriptiva.ForeColor = System.Drawing.Color.White;
+            this.btnDescriptiva.Location = new System.Drawing.Point(0, 0);
+            this.btnDescriptiva.Name = "btnDescriptiva";
+            this.btnDescriptiva.Size = new System.Drawing.Size(248, 40);
+            this.btnDescriptiva.TabIndex = 0;
+            this.btnDescriptiva.Text = "Analizar una variable";
+            this.btnDescriptiva.UseVisualStyleBackColor = false;
+            this.btnDescriptiva.Click += new System.EventHandler(this.btnDescriptiva_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.panelSubDescriptiva);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.topPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 561);
+            this.panel1.TabIndex = 14;
             // 
             // frmMain
             // 
@@ -214,7 +214,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1008, 600);
+            this.ClientSize = new System.Drawing.Size(1008, 561);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -223,30 +223,29 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmMain";
             this.Load += new System.EventHandler(this.frmMain_Load);
-            this.topPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
+            this.topPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelSubDescriptiva.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnDescriptiva;
-        private System.Windows.Forms.Panel topPanel1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Panel topPanel2;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel parentPanel;
+        private System.Windows.Forms.Panel topPanel1;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panelSubDescriptiva;
+        private System.Windows.Forms.Button btnDescriptiva;
+        private System.Windows.Forms.Panel panel1;
     }
 }
